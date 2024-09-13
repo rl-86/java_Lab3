df12b492cce5c895478e81d4baa08ab4c9b86ecf
rl-86 <rl86@live.se>
2024-09-13 19:09:38 +0200

    Moving folders

---
+++ src/service/java_lab3/Warehouse.java
@@ -0,0 +1,51 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Warehouse {
+
+    public static Scanner sc = new Scanner(System.in);
+    public static String goBack = ("\nTryck \"Enter\" för att gå till huvudmenyn");
+
+    public static void main(String[] args) {
+
+        String choice;
+        do {
+            //Main menu
+            System.out.println("\nWarehouse");
+            System.out.println("========");
+            System.out.println("1. Inmatning");
+            System.out.println("2. ");
+            System.out.println("3. ");
+            System.out.println("4. ");
+            System.out.println("e. Avsluta");
+            System.out.println("\nAnge menyval + enter:");
+
+
+            choice = sc.nextLine();
+
+            switch (choice) {
+
+                case "1":
+
+                    System.out.println(goBack);
+                    sc.nextLine();
+                    break;
+                case "2":
+
+                    break;
+
+                case "3":
+                    break;
+
+                default:
+
+            }
+        } while (!choice.equalsIgnoreCase("e"));
+
+        System.out.println("Avslutar...");
+
+
+    }
+}
--- src/test/java_lab3/WarehouseTest.java
+++ src/test/java_lab3/WarehouseTest.java
@@ -1,5 +1,16 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class WarehouseTest {
+
+
+    @Test
+    @DisplayName("Add a and b")
+    void add() {
+        var result = Warehouse.add(2,3);
+        assertEquals(5,result);
+    }
 
 }
